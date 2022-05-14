908b20bd4233519c406dff4f3e4ed8f46e59b098
Ricardo Lopez <81579356+mrlopezharo@users.noreply.github.com>
2022-05-14 17:23:47 -0600

    Tutorial TypeScript con Node.js y Express.
    Tutorial de Midudev

---
+++ src/routes/diaries.ts
@@ -0,0 +1,31 @@
+import express from 'express'// ESModules
+import * as diaryServices from '../services/diaryServices'
+
+const router = express.Router()
+
+router.get('/', (_req, res) =>{
+    res.send(diaryServices.getEntriesWithoutSensitiveInfo())
+})
+
+router.get('/:id', (req, res) =>{
+    const diary = diaryServices.findById(+req.params.id)
+
+    return (diary != null)
+        ? res.send(diary)
+        : res.sendStatus(404)
+})
+
+router.post('/', (req, res) =>{
+    const { date, weather, visibility, comment} = req.body
+    
+    const newDiaryEntry = diaryServices.addDiary({
+        date,
+        weather,
+        visibility,
+        comment
+    })
+
+    res.json(newDiaryEntry)
+})
+
+export default router
